b251b73c5a4e69ef26f8fa22c386e12fa74896de
raonytog <raony.gomes@edu.ufes.br>
2024-06-29 20:46:18 -0300

    ajustes na leitura e salvamento

--- compacta.c
+++ compacta.c
@@ -2,27 +2,41 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 256
+#define MAX_ASCI 256  //tamanho maximo do vetor com as qts de uso de cada letra
+#define MAX_TEXT 4096 // tamanho maximo do vetor q armazena o texto
 
-void PreencheVetor(int *vetor) {
+void *PreencheVetor(int *vetor, char *text) {
     char letra = '\0';
+    int idx = 0;
     FILE *fText = fopen("texto.txt", "r");
     while ( !feof(fText) ) {
+        letra = '\0';
         if (fscanf(fText, "%c", &letra) == 1) 
             vetor[letra]++;
+            text[idx++] = letra;
     }
+    text[idx] = '\0';
 }
 
 void PrintVetor(int *vetor) {
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < MAX_ASCI; i++)
         printf("%c: %d\n", i, vetor[i]);
     printf("\n");
 }
 
-int main () {
-    int *vetor = calloc(MAX, sizeof(int));
-    PreencheVetor(vetor);
-    PrintVetor(vetor);
+int Compara(const void *a, const void *b) {
+   return ( *(int*)b - *(int*)a );
+}
 
+int main () {
+    // armazena as qts usadas de cada caractere
+    int *vetor = calloc(MAX_ASCI, sizeof(int));
+
+    // armazena o texto original
+    char text[MAX_TEXT] = "\0";
+
+    PreencheVetor(vetor, text);
+    //qsort(vetor, MAX_ASCI, sizeof(vetor[0]), Compara); 
+    PrintVetor(vetor);
     return 0;
 }
